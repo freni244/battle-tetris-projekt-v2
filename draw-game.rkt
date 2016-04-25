e6a2b0fc824498c3d6cb8ab2a58f8100ca2ac950
Fredrik Nilsson <freni244@student.liu.se>
2016-04-25 18:49:17 +0200

    Timers och annat

---
+++ draw-game.rkt
@@ -0,0 +1,109 @@
+#lang racket/gui
+(require "block.rkt")
+(require "board.rkt")
+(require "game-init.rkt")
+(require "cmd_store.rkt")
+
+(define *window* (new frame%
+                     [label "window"]
+                     [width 900]
+                     [height 600]
+                     [x 0]	 
+                     [y 0]))
+(send *window* show #t)
+
+(define (draw-grid canvas dc x y width height color)
+  (send dc draw-rectangle x y width height) ;; x y width height
+  (send dc set-brush color 'solid)
+  (send dc set-pen "black" 2 'solid))
+
+(define (draw-board canvas dc) ;; hämta och ritar board från game-init
+  (draw-board-help canvas dc 100 100 (send *board-1* get-matrix)))
+
+(define (draw-board-help canvas dc x y items)
+  (cond
+    ((empty? (cdr items))
+     (draw-lines canvas dc (car items) x y))
+    (else (begin (draw-lines canvas dc (car items) x y)
+                 (draw-board-help canvas dc x (+ 20 y) (cdr items))))))
+
+(define (draw-lines canvas dc items x y)
+  (cond
+    ((empty? (cdr items))
+     ;(draw-grid canvas dc x y 20 20 "white"))
+     (cond ((= (car items) 0)
+            (begin (draw-grid canvas dc x y 20 20 "white")))
+           ((= (car items) 1)
+            (begin (draw-grid canvas dc x y 20 20 "lime"))))) ;; behövs fler villkor..? eller bara white
+    
+    ((= (car items) 0)
+     (begin (draw-grid canvas dc x y 20 20 "white")
+            (draw-lines canvas dc (cdr items) (+ x 20) y)))
+    ((= (car items) 1)
+     (begin (draw-grid canvas dc x y 20 20 "lime")
+            (draw-lines canvas dc (cdr items) (+ x 20) y)))
+    ((= (car items) 2)
+     (begin (draw-grid canvas dc x y 20 20 "blue")
+            (draw-lines canvas dc (cdr items) (+ x 20) y)))
+    ((= (car items) 3)
+     (begin (draw-grid canvas dc x y 20 20 "red")
+            (draw-lines canvas dc (cdr items) (+ x 20) y)))
+    ((= (car items) 4)
+     (begin (draw-grid canvas dc x y 20 20 "yellow")
+            (draw-lines canvas dc (cdr items) (+ x 20) y)))
+    ((= (car items) 5)
+     (begin (draw-grid canvas dc x y 20 20 "orange")
+            (draw-lines canvas dc (cdr items) (+ x 20) y)))
+    ((= (car items) 6)
+     (begin (draw-grid canvas dc x y 20 20 "cyan")
+            (draw-lines canvas dc (cdr items) (+ x 20) y)))
+    ((= (car items) 7)
+     (begin (draw-grid canvas dc x y 20 20 "magenta")
+            (draw-lines canvas dc (cdr items) (+ x 20) y)))))
+
+(define (random-color)
+  (let ((color-list '("blue" "red" "yellow" "orange" "lime" "magenta" "cyan")))
+    (car (shuffle color-list))))
+
+;; (Ritar block givet lista av blockets koordinater (tex (send *I* get-place)). Inargument: canvas dc block)
+;;;;;; iggnorera då vi inte kommer att behöva denna!
+(define (draw-block canvas block-dc block color)
+  (let ((part1 (first block))
+        (part2 (second block))
+        (part3 (third block))
+        (part4 (fourth block)))
+    (send block-dc set-brush color 'solid)
+    (send block-dc draw-rectangle (+ 100 (* (- (car part1) 1) 20)) (+ 100 (* (- (cadr part1) 1) 20)) 20 20) ;; alla fyra delar bildar ett helt block   ; (car part1) (cadr part1) 20 20)
+    (send block-dc draw-rectangle (+ 100 (* (- (car part2) 1) 20)) (+ 100 (* (- (cadr part2) 1) 20)) 20 20)
+    (send block-dc draw-rectangle (+ 100 (* (- (car part3) 1) 20)) (+ 100 (* (- (cadr part3) 1) 20)) 20 20)
+    (send block-dc draw-rectangle (+ 100 (* (- (car part4) 1) 20)) (+ 100 (* (- (cadr part4) 1) 20)) 20 20)))
+
+
+    
+(define (draw-cycle canvas dc); Stoppa in alla saker som ska ritas i denna.
+  (draw-board canvas dc)
+  (draw-block canvas dc (send *I* get-place) (random-color)))
+  ;(send *a-canvas* refresh-now))
+ ; (send *a-canvas* refresh-now))
+
+(define (refresh-draw-cycle)
+  (send *a-canvas* refresh-now))
+
+(define *draw-timer* (new timer%
+                     [notify-callback refresh-draw-cycle]))
+(send *draw-timer* start 60 #f)
+
+(define (draw-fall)
+  (send *T* move-down))
+  
+
+(define *fall-timer* (new timer%
+                     [notify-callback draw-fall]))
+(send *fall-timer* start 1000 #f)
+
+(define *a-canvas*
+  (new canvas%
+       [parent *window*]
+       [paint-callback draw-cycle]))
+
+(provide (all-defined-out))
--- game-init.rkt
+++ game-init.rkt
@@ -1,9 +1,71 @@
 #lang racket
 (require "block.rkt")
 (require "board.rkt")
-(require "interaction-utils.rkt") ; ...
 (require "cmd_store.rkt")
 
 (define *I*
   (new block%
-       ;[shape '((- c 1))]
+       [coordinates '((4 1) (5 1) (6 1) (7 1))]
+       [type 'I]
+       [color 1]))
+
+(define *J*
+  (new block%
+       [coordinates '((4 1) (5 1) (6 1) (6 2))]
+       [type 'J]
+       [color 2]))
+
+(define *L*
+  (new block%
+       [coordinates '((4 1) (5 1) (6 1) (4 2))]
+       [type 'L]
+       [color 3]))
+
+(define *O*
+  (new block%
+       [coordinates '((4 1) (5 1) (4 2) (5 2))]
+       [type 'O]
+       [color 4]))
+
+(define *S*
+  (new block%
+       [coordinates '((5 1) (6 1) (4 2) (5 2))]
+       [type 'S]
+       [color 5]))
+
+(define *T*
+  (new block%
+       [coordinates '((4 1) (5 1) (6 1) (5 2))]
+       [type 'T]
+       [color 6]))
+
+(define *Z*
+  (new block%
+       [coordinates '((4 1) (5 1) (5 2) (6 2))]
+       [type 'Z]
+       [color 7]))
+
+(define *board-1*
+  (new board%
+       [matrix (list (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 1 1 1 1 1 1 1 1 1 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0)
+                     (list 0 0 0 0 0 0 0 0 0 0))]))
+
+(provide (all-defined-out))
